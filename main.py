4c213e389141f49b82ac4770fbce2076e523c133
Rakha Djokosoetono <rakhadjo@gmail.com>
2020-07-21 03:47:19 +0700

    attempted to add cat pictures

--- main.py
+++ main.py
@@ -4,19 +4,31 @@ import re
 
 from config import key
 
-def get_url(): 
+def get_url_dog(): 
 	contents = requests.get('https://random.dog/woof.json').json()
 	return contents['url']
 
 def bop(bot, update):
-	url = get_url()
+	url = get_url_dog()
 	chat_id = update.message.chat_id
 	bot.send_photo(chat_id=chat_id, photo=url)
+
+def get_url_cat():
+    contents = requests.get('https://api.thecatapi.com/v1/images/search').json()[0]
+    print(contents)
+    return contents
+
+def maw(bot, update):
+    url = get_url_cat()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
 
 def main():
     updater = Updater(key)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('bop',bop))
+    dp.add_handler(CommandHandler('maw',maw))
     updater.start_polling()
     updater.idle()
     
